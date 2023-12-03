d89078783bb2aa7cb0d15edf8b466868757b7f15
kotgire58 <144971873+kotgire58@users.noreply.github.com>
2023-12-03 18:30:06 -0500

    Update Book.java
    Kamal

--- src/main/java/com/oodProject/library/pojo/Book.java
+++ src/main/java/com/oodProject/library/pojo/Book.java
@@ -1,5 +1,40 @@
 package com.oodProject.library.pojo;
 
 public class Book {
+    private String title;
+    private String author;
+    private boolean isAvailable;
 
+    public Book(String title, String author) {
+        this.title = title;
+        this.author = author;
+        this.isAvailable = true;
+    }
+
+    // Getters and setters
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public boolean isAvailable() {
+        return isAvailable;
+    }
+
+    public void setAvailable(boolean available) {
+        isAvailable = available;
+    }
+
+    // Method to display book details
+    public void displayDetails() {
+        System.out.println("Title: " + title + ", Author: " + author + ", Available: " + isAvailable);
+    }
 }
+
+
+  
+
+
