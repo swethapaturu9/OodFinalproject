83489a0e9dd6092a3c5c30b9639755378d6a5d4d
kotgire58 <144971873+kotgire58@users.noreply.github.com>
2023-12-03 18:32:17 -0500

    Update PrivateRoom.java
    Kamal

--- src/main/java/com/oodProject/library/pojo/PrivateRoom.java
+++ src/main/java/com/oodProject/library/pojo/PrivateRoom.java
@@ -1,5 +1,28 @@
 package com.oodProject.library.pojo;
 
 public class PrivateRoom {
+    private int roomNumber;
+    private boolean isOccupied;
 
+    public PrivateRoom(int roomNumber) {
+        this.roomNumber = roomNumber;
+        this.isOccupied = false;
+    }
+
+    public int getRoomNumber() {
+        return roomNumber;
+    }
+
+    public boolean isOccupied() {
+        return isOccupied;
+    }
+
+    public void setOccupied(boolean occupied) {
+        isOccupied = occupied;
+    }
+
+    public void displayStatus() {
+        System.out.println("Room Number: " + roomNumber + ", Occupied: " + isOccupied);
+    }
+}
 }
